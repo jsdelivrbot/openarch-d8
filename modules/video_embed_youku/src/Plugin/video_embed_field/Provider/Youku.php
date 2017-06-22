d9d19cf19fd3d1db1bac445d13a9c5ad1d5296e0
Josh <joshuaadamparker@hotmail.com>
2017-06-22 00:04:38 -0700

    add fallback for video embeds

--- modules/video_embed_youku/src/Plugin/video_embed_field/Provider/Youku.php
+++ modules/video_embed_youku/src/Plugin/video_embed_field/Provider/Youku.php
@@ -24,12 +24,19 @@ class Youku extends ProviderPluginBase {
     return [
       '#type' => 'html_tag',
       '#tag' => 'iframe',
+      // '#query' => [
+      //   'client_id' => '17ef1892ecbd1f6e',
+      // ],
       '#attributes' => [
+        // 'client_id' => '17ef1892ecbd1f6e',
         'width' => $width,
         'height' => $height,
         'frameborder' => '0',
         'allowfullscreen' => 'allowfullscreen',
-        'src' => sprintf('http://player.youku.com/embed/%s&autoplay=%d', $this->getVideoId(), $autoplay),
+        //'src' => sprintf('http://player.youku.com/embed/%s&autoplay=%d', $this->getVideoId(), $autoplay),
+        //'src' => sprintf('http://player.youku.com/embed/%s&autoplay=%d/partnerid/17ef1892ecbd1f6e', $this->getVideoId(), $autoplay),
+        'src' => sprintf('http://player.youku.com/embed/%s?client_id=17ef1892ecbd1f6e', $this->getVideoId()),
+        //http://player.youku.com/player.php/sid/XMjc4NDkyNDE4NA==/isAutoPlay/false/newPlayer/true/partnerid/17ef1892ecbd1f6e/v.swf
       ],
     ];
   }
@@ -37,6 +44,10 @@ class Youku extends ProviderPluginBase {
   /**
    * {@inheritdoc}
    */
+  // public function getRemoteThumbnailUrl() {
+  //   $video_data = json_decode(file_get_contents('https://openapi.youku.com/v2/videos/show_basic.json?client_id=8d025b9c897b22a8&video_id=s%', $this->getVideoId()));
+  //   return $video_data->thumbnail;
+  // }
   public function getRemoteThumbnailUrl() {
     $video_data = json_decode(file_get_contents('https://openapi.youku.com/v2/videos/show_basic.json?client_id=8d025b9c897b22a8&video_id=s%', $this->getVideoId()));
     return $video_data->thumbnail;
